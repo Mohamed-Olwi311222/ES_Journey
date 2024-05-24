922246f69d73b5ab2acabfefde4416ff014c8a27
Mohamed Olwi <acemido2004@gmail.com>
2024-05-24 15:30:41 +0300

    added button project with its test functions and renamed the proteus project of the dc motor

---
+++ Application/Application.X/build/default/debug/TESTS/button_test/button_test.p1.d
@@ -0,0 +1,17 @@
+build/default/debug/TESTS/button_test/button_test.p1:  \
+TESTS/button_test/button_test.c  \
+TESTS/button_test/../../app.h  \
+ECU_Layer/led/ecu_led.h  \
+ECU_Layer/led/../../MCAL_Layer/GPIO/hal_gpio.h  \
+ECU_Layer/led/../../MCAL_Layer/GPIO/../mcal_std_types.h  \
+ECU_Layer/led/../../MCAL_Layer/GPIO/../../std_types.h  \
+ECU_Layer/led/../../MCAL_Layer/GPIO/../../compiler.h  \
+ECU_Layer/led/../../MCAL_Layer/GPIO/../../device_config.h  \
+ECU_Layer/led/../../MCAL_Layer/GPIO/hal_gpio_cfg.h  \
+ECU_Layer/led/ecu_led_cfg.h  \
+ECU_Layer/button/ecu_button.h  \
+ECU_Layer/button/ecu_button_cfg.h  \
+ECU_Layer/Relay/ecu_relay.h  \
+ECU_Layer/Relay/ecu_relay_cfg.h  \
+ECU_Layer/DC_Motor/ecu_dc_motor.h  \
+ECU_Layer/DC_Motor/ecu_dc_motor_cfg.h 
