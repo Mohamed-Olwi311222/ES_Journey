71b1dcbb7592374f869558cfba65e7c9fda67361
Mohamed Olwi <acemido2004@gmail.com>
2024-08-03 03:32:36 +0200

    Added more micro functions and made a struct for each type of external interrupt , an enum for rising and falling edge and an enum for identifying which INTx caused the interrupt and finnally software interfaces to interface with the external interrupt module driver

--- Application/Application.X/MCAL_Layer/Interrupt/mcal_external_interrupt.h
+++ Application/Application.X/MCAL_Layer/Interrupt/mcal_external_interrupt.h
@@ -9,10 +9,12 @@
 #define	MCAL_EXTERNAL_INTERRUPT_H
 /*----------------------------Header Files------------------------------------*/
 #include "mcal_interrupt_config.h"
-
+#include "../GPIO/hal_gpio.h"
 /*----------------------------Macros Declarations-----------------------------*/
 
 /*----------------------------Macros Functions Declarations-------------------*/
+#if EXTERNAL_INTERRUPT_ENABLE == INTERRUPT_FEATURE_ENABLE
+
 #if EXTERNAL_INTERRUPT_INTx_FEATURE_ENABLE == INTERRUPT_FEATURE_ENABLE
 
 /******INT0******/
@@ -103,29 +105,110 @@
 
 #endif
 #endif
+
 #if EXTERNAL_INTERRUPT_ONChange_FEATURE_ENABLE == INTERRUPT_FEATURE_ENABLE
 /**
  * ENABLE PORTB On change interrupt
  */
-#define EXT_ON_CHANGE_INTERRUPT_ENABLE                  (INTCONbits.RBIE = 1)
+#define EXT_RBx_INTERRUPT_ENABLE                        (INTCONbits.RBIE = 1)
 /**
  * DISABLE PORTB On change interrupt
  */
-#define EXT_ON_CHANGE_INTERRUPT_DISABLE                 (INTCONbits.RBIE = 0)
+#define EXT_RBx_INTERRUPT_DISABLE                       (INTCONbits.RBIE = 0)
 /**
  * Clear PORTB on change interrupt flag bit
  */
-#define EXT_ON_CHANGE_INTERRUPT_FLAG_BIT_CLEAR          (INTCONbits.RBIF = 0)
+#define EXT_RBx_INTERRUPT_FLAG_BIT_CLEAR                (INTCONbits.RBIF = 0)
+
 #if INTERRUPT_PRIORITY_LEVELS_ENABLE == INTERRUPT_FEATURE_ENABLE
 /**
  * On change interrupt HIGH priority
  */
+#define EXT_RBx_INTERRUPT_HIGH_PRIORITY                 (INTCON2.RBIP = 1)
+/**
+ * On change interrupt LOW priority
+ */
+#define EXT_RBx_INTERRUPT_LOW_PRIORITY                  (INTCON2.RBIP = 0)
+#endif
 
 #endif
-#endif
 
+#endif
 /*----------------------------DataTypes---------------------------------------*/
 
+/**
+ * an enum for external interrupts for contolling the falling and rising edges
+ */
+typedef enum
+{
+    INTERRUPT_FALLING_EDGE = 0,                         /*Interrupt on falling edge*/
+    INTERRUPT_RISING_EDGE = 1                           /*Interrupt on rising edge*/
+}interrupt_INTx_edge;
+
+/**
+ * an enum for external interrupts for identifying which INTx caused the interrupt
+ */
+typedef enum
+{
+    INTERRUPT_EXTERNAL_INT0 = 0,                        /*INT0 interrupt is the source*/
+    INTERRUPT_EXTERNAL_INT1 = 1,                        /*INT1 interrupt is the source*/
+    INTERRUPT_EXTERNAL_INT2 = 2,                        /*INT2 interrupt is the source*/
+}interrupt_INTx_src;
+
+/**
+ * struct interrupt_INTx_t - a struct for external interrupts
+ * @EXT_interrupt_handler:
+ * @mcu_pin: the mcu pin that caused the interrupt
+ * @edge: falling or rising edge
+ * @source: the source of the interrupt
+ * @priority: the priority of the interrupt
+ */
+typedef struct
+{
+    void (*EXT_interrupt_handler)(void);
+    pin_config_t mcu_pin;
+    interrupt_INTx_edge edge;
+    interrupt_INTx_src source;
+    interrupt_priority_cfg priortiy;
+}interrupt_INTx_t;
+
+/**
+ * struct interrupt_RBYx_t - a struct for PORTB on change interrupt
+ * @mcu_pin: the mcu pin that caused the interrupt
+ * @priortiy: the priority of the RB on change interrupt
+ */
+typedef struct
+{
+    void (* EXT_interrupt_handler)(void);
+    pin_config_t mcu_pin;
+    interrupt_priority_cfg priortiy;
+}interrupt_RBYx_t;
 /*----------------------------Function Prototypes-----------------------------*/
+
+/**
+ * @brief: A software interface to initialize the given external INTx interrupt
+ * @param int_obj the external INTx interrupt object
+ * @return E_OK if success otherwise E_NOT_OK
+ */
+Std_ReturnType Interrupt_INTx_Init(const interrupt_INTx_t *int_obj);
+/**
+ * @brief: A software interface to deinitialize the given external INTx interrupt
+ * @param int_obj the external INTx interrupt object
+ * @return E_OK if success otherwise E_NOT_OK
+ */
+Std_ReturnType Interrupt_INTx_Deinit(const interrupt_INTx_t *int_obj);
+/**
+ * @brief: A software interface to initialize the PORTB on change interrupt
+ * @param int_obj the PORTB on change interrupt object
+ * @return E_OK if success otherwise E_NOT_OK
+ */
+Std_ReturnType Interrupt_RBx_Init(const interrupt_INTx_t *int_obj);
+/**
+ * @brief: A software interface to deinitialize the PORTB on change interrupt
+ * @param int_obj the PORTB on change interrupt object
+ * @return E_OK if success otherwise E_NOT_OK
+ */
+Std_ReturnType Interrupt_RBx_Deinit(const interrupt_INTx_t *int_obj);
+
 #endif	/* MCAL_EXTERNAL_INTERRUPT_H */
 
