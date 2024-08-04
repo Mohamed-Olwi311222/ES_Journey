ef620f25e4288a78f303ec3dea216b7c22961fa4
Mohamed Olwi <acemido2004@gmail.com>
2024-08-04 09:42:01 +0200

    Added the implementation of the interrupt manager if the interrupt priority feature is disableda and with also precompile check if its enabled(soon the other 2 managers will be implemented

--- Application/Application.X/MCAL_Layer/Interrupt/mcal_interrupt_manager.c
+++ Application/Application.X/MCAL_Layer/Interrupt/mcal_interrupt_manager.c
@@ -5,3 +5,27 @@
  * Created on 01 August 2024, 06:51
  */
 #include "mcal_interrupt_manager.h"
+#if INTERRUPT_PRIORITY_LEVELS_ENABLE == INTERRUPT_FEATURE_ENABLE
+/**
+ * @brief the interrupt manager for high priority interrupts
+ */
+void __interrupt() Interrupt_Manager_High(void)
+{
+
+}
+/**
+ * @brief the interrupt manager for low priority interrupts
+ */
+void  __interrupt(low_priority) Interrupt_Manager_Low(void)
+{
+
+}
+ #else
+void __interrupt() Interrupt_Manager(void)
+{
+    if ((INTERRUPT_ENABLE == INTCONbits.INT0IE) && (INTERRUPT_OCCUR == INTCONbits.INT0IF))
+    {
+        INT0_ISR(); // Call the ISR function
+    }
+}
+#endif
