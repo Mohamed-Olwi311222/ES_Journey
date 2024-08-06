783e8a392f181157c5e91ee2ef94f9cd59db8f89
Mohamed Olwi <acemido2004@gmail.com>
2024-08-06 12:25:16 +0200

    Implemented the functions to support interrupt priority feature as intended

--- Application/Application.X/MCAL_Layer/Interrupt/mcal_interrupt_manager.c
+++ Application/Application.X/MCAL_Layer/Interrupt/mcal_interrupt_manager.c
@@ -11,14 +11,24 @@
  */
 void __interrupt() Interrupt_Manager_High(void)
 {
-
+    if ((INTERRUPT_ENABLE == INTCONbits.INT0IE) && (INTERRUPT_OCCUR == INTCONbits.INT0IF))
+    {
+        INT0_ISR(); // Call the ISR function
+    }
+    if ((INTERRUPT_ENABLE == INTCON3bits.INT2IE) && (INTERRUPT_OCCUR == INTCON3bits.INT2IF))
+    {
+        INT2_ISR(); // Call the ISR function
+    }
 }
 /**
  * @brief the interrupt manager for low priority interrupts
  */
 void  __interrupt(low_priority) Interrupt_Manager_Low(void)
 {
-
+    if ((INTERRUPT_ENABLE == INTCON3bits.INT1IE) && (INTERRUPT_OCCUR == INTCON3bits.INT1IF))
+    {
+        INT1_ISR(); // Call the ISR function
+    }  
 }
  #else
 void __interrupt() Interrupt_Manager(void)
@@ -26,6 +36,14 @@ void __interrupt() Interrupt_Manager(void)
     if ((INTERRUPT_ENABLE == INTCONbits.INT0IE) && (INTERRUPT_OCCUR == INTCONbits.INT0IF))
     {
         INT0_ISR(); // Call the ISR function
+    }
+    if ((INTERRUPT_ENABLE == INTCON3bits.INT1IE) && (INTERRUPT_OCCUR == INTCON3bits.INT1IF))
+    {
+        INT1_ISR(); // Call the ISR function
+    }
+    if ((INTERRUPT_ENABLE == INTCON3bits.INT2IE) && (INTERRUPT_OCCUR == INTCON3bits.INT2IF))
+    {
+        INT2_ISR(); // Call the ISR function
     }
 }
 #endif
