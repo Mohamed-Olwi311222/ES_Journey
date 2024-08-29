f024c0c1340a51916fa10ddbc288016721cfee8f
Mohamed Olwi <acemido2004@gmail.com>
2024-08-30 00:01:01 +0200

    Added a header file for adc configs

---
+++ Application/Application.X/MCAL_Layer/ADC/hal_adc_cfg.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   hal_adc_cfg.h
+ * Author: Mohamed olwi
+ *
+ * Created on 29 August 2024, 23:43
+ */
+
+#ifndef HAL_ADC_CFG_H
+#define	HAL_ADC_CFG_H
+/*----------------------------Header Files------------------------------------*/
+
+/*----------------------------Macros Declarations-----------------------------*/
+
+/*----------------------------Macros Functions Declarations-------------------*/
+
+/*----------------------------DataTypes---------------------------------------*/
+
+/*----------------------------Function Prototypes-----------------------------*/
+
+#endif	/* HAL_ADC_CFG_H */
+
