f73ee4aa9bdf2f759719668e34c1b818ea354fea
Mohamed Olwi <acemido2004@gmail.com>
2024-09-02 14:41:21 +0200

    Added Macro functions to handle adc interrupt and its priority with precompile configurations

--- Application/Application.X/MCAL_Layer/Interrupt/mcal_internal_interrupt.h
+++ Application/Application.X/MCAL_Layer/Interrupt/mcal_internal_interrupt.h
@@ -13,6 +13,30 @@
 /*----------------------------Macros Declarations-----------------------------*/
 
 /*----------------------------Macros Functions Declarations-------------------*/
+#if ADC_INTERRUPT_FEATURE == INTERRUPT_FEATURE_ENABLE
+/**
+ * Clears the interrupt enable for the ADC module
+ */
+#define ADC_INTERRUPT_DISABLE()                     (PIE1bits.ADIE = 0)
+/**
+ * Sets the interrupt enable for the ADC module
+ */
+#define ADC_INTERRUPT_ENABLE()                      (PIE1bits.ADIE = 1)
+/**
+ * Clears the interrupt flag for the ADC module
+ */
+#define ADC_INTERRUPT_FLAG_BIT_CLEAR()             (PIR1bits.ADIF = 0)
+#if INTERRUPT_PRIORITY_LEVELS_ENABLE == INTERRUPT_FEATURE_ENABLE
+/**
+ * ADC HIGH priority
+ */
+#define ADC_HIGH_PRIORITY()                        (IPR1bits.ADIP = 1)
+/**
+ * ADC LOW priority
+ */
+#define ADC_LOW_PRIORITY()                         (IPR1bits.ADIP = 0)
+#endif
+#endif
 
 /*----------------------------DataTypes---------------------------------------*/
 
