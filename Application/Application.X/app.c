854fac5951ddc3886d61b3c67cb5e181d4dc8d45
Mohamed Olwi <acemido2004@gmail.com>
2024-09-04 22:00:30 +0200

    Added a new project using LM35 temperature sensor and 2 dc motors connected with l298 motor driver , the first motor starts to move right if sensor 1 temperature is above 20 and stops otherwise; the second motor starts to move if sensor 2 temperature is above 25 and stops otherwise, the LM35 each celsius is 10mv so the result given by the adc will be multiplied by 4.88(step size which is 5/2^10 - 1) and then divided by 10 to measure the actual celsius

--- Application/Application.X/app.c
+++ Application/Application.X/app.c
@@ -8,18 +8,36 @@
 #include "MCAL_Layer/ADC/hal_adc.h"
 
 void adc_interrupt_handler(void);
-adc_conversion_result adc_result1 = ZERO_INIT;
-adc_conversion_result adc_result2 = ZERO_INIT;
-adc_conversion_result adc_result3 = ZERO_INIT;
-adc_conversion_result adc_result4 = ZERO_INIT;
+adc_conversion_result lm35_result1 = ZERO_INIT;
+adc_conversion_result lm35_result2 = ZERO_INIT;
+uint16 lm35_res1_celsius_mv = ZERO_INIT;
+uint16 lm35_res2_celsius_mv = ZERO_INIT;
 
-uint8 adc_result1_txt[6];
-uint8 adc_result2_txt[6];
-uint8 adc_result3_txt[6];
-uint8 adc_result4_txt[6];
+uint8 lm35_result1_txt[6];
+uint8 lm35_result2_txt[6];
 
 uint8 channel_select = ZERO_INIT;
 
+dc_motor_t motor1 = {
+  .dc_motor_pins[0].direction = GPIO_DIRECTION_OUTPUT,
+  .dc_motor_pins[0].logic = GPIO_LOW,
+  .dc_motor_pins[0].port = PORTD_INDEX,
+  .dc_motor_pins[0].pin = GPIO_PIN0,
+  .dc_motor_pins[1].direction = GPIO_DIRECTION_OUTPUT,
+  .dc_motor_pins[1].logic = GPIO_LOW,
+  .dc_motor_pins[1].port = PORTD_INDEX,
+  .dc_motor_pins[1].pin = GPIO_PIN1,
+};
+dc_motor_t motor2 = {
+  .dc_motor_pins[0].direction = GPIO_DIRECTION_OUTPUT,
+  .dc_motor_pins[0].logic = GPIO_LOW,
+  .dc_motor_pins[0].port = PORTD_INDEX,
+  .dc_motor_pins[0].pin = GPIO_PIN2,
+  .dc_motor_pins[1].direction = GPIO_DIRECTION_OUTPUT,
+  .dc_motor_pins[1].logic = GPIO_LOW,
+  .dc_motor_pins[1].port = PORTD_INDEX,
+  .dc_motor_pins[1].pin = GPIO_PIN3,
+};
 char_lcd_4bit_t lcd_1 = {
     .lcd_rs.port = PORTC_INDEX,
     .lcd_rs.pin = GPIO_PIN0,
@@ -46,13 +64,12 @@ char_lcd_4bit_t lcd_1 = {
     .lcd_data[3].direction = GPIO_DIRECTION_OUTPUT,
     .lcd_data[3].logic = GPIO_LOW
 };
-
 adc_config_t adc = {
   .adc_interrupt_handler = adc_interrupt_handler,
   .priortiy = INTERRUPT_HIGH_PRIORITY,
   .aquisition_time = ADC_ACQUISITION_TIME_12_TAD,
   .adc_analog_channel = ADC_CHANNEL_AN0,
-  .adc_port_config = ADC_AN0__AN3__ANALOG_FUNCTIONALITY,
+  .adc_port_config = ADC_AN0__AN1__ANALOG_FUNCTIONALITY,
   .conversion_clock = ADC_CONVERSION_CLOCK_FOSC16,
   .result_format = ADC_RES_RIGHT_JUSTIFY,
   .voltage_ref = ADC_VREF_INTENAL
@@ -62,22 +79,12 @@ void adc_interrupt_handler(void)
 {
     if (ADC_CHANNEL_AN0 == channel_select)
     {
-        adc_read_result(&adc, &adc_result1);
+        adc_read_result(&adc, &lm35_result1);
         channel_select = ADC_CHANNEL_AN1;
-    }
-    else if (ADC_CHANNEL_AN1 == channel_select)
-    {
-        adc_read_result(&adc, &adc_result2);
-        channel_select = ADC_CHANNEL_AN2;
-    }
-    else if (ADC_CHANNEL_AN2 == channel_select)
-    {
-        adc_read_result(&adc, &adc_result3);
-        channel_select = ADC_CHANNEL_AN3;
     }
     else
     {
-        adc_read_result(&adc, &adc_result4);
+        adc_read_result(&adc, &lm35_result2);
         channel_select = ADC_CHANNEL_AN0;
     }
 }
@@ -91,13 +98,14 @@ int main(void)
     }
     ret |= adc_init(&adc);
     ret |= lcd_4bit_initialize(&lcd_1);
-    ret |= lcd_4bit_send_string_pos(&lcd_1, 1, 6, "ADC test");
+    ret |= dc_motor_initialize(&motor1);
+    ret |= dc_motor_initialize(&motor2);
+    
+    ret |= lcd_4bit_send_string_pos(&lcd_1, 1, 6, "LM35 test");
     __delay_ms(2000);
     ret |= lcd_4bit_send_command(&lcd_1, _LCD_CLEAR);
-    ret |= lcd_4bit_send_string_pos(&lcd_1, 1, 1, "POT1: ");
-    ret |= lcd_4bit_send_string_pos(&lcd_1, 2, 1, "POT2: ");
-    ret |= lcd_4bit_send_string_pos(&lcd_1, 3, 1, "POT3: ");
-    ret |= lcd_4bit_send_string_pos(&lcd_1, 4, 1, "POT4: ");
+    ret |= lcd_4bit_send_string_pos(&lcd_1, 1, 1, "Sensor1: ");
+    ret |= lcd_4bit_send_string_pos(&lcd_1, 2, 1, "Sensor2: ");
 
     while(1)
     {
@@ -105,28 +113,38 @@ int main(void)
         {
             ret |= adc_get_conversion_interrupt(&adc, ADC_CHANNEL_AN0);
         }
-        else if (ADC_CHANNEL_AN1 == channel_select)
+        else
         {
             ret |= adc_get_conversion_interrupt(&adc, ADC_CHANNEL_AN1);
         }
-        else if (ADC_CHANNEL_AN2 == channel_select)
+        lm35_res1_celsius_mv = lm35_result1 * 4.88f;
+        lm35_res2_celsius_mv = lm35_result2 * 4.88f;
+
+        lm35_res1_celsius_mv /= 10;
+        lm35_res2_celsius_mv /= 10;
+
+        ret |= convert_uint16_to_string(lm35_res1_celsius_mv, lm35_result1_txt);
+        ret |= convert_uint16_to_string(lm35_res2_celsius_mv, lm35_result2_txt);
+
+        ret |= lcd_4bit_send_string_pos(&lcd_1, 1, 9, lm35_result1_txt);
+        ret |= lcd_4bit_send_string_pos(&lcd_1, 2, 9, lm35_result2_txt);
+        if (lm35_res1_celsius_mv > 20)
         {
-            ret |= adc_get_conversion_interrupt(&adc, ADC_CHANNEL_AN2);
+            ret |= dc_motor_move_forward(&motor1);
         }
         else
         {
-            ret |= adc_get_conversion_interrupt(&adc, ADC_CHANNEL_AN3);
+            ret |= dc_motor_stop(&motor1);
         }
         
-        ret |= convert_uint16_to_string(adc_result1, adc_result1_txt);
-        ret |= convert_uint16_to_string(adc_result2, adc_result2_txt);
-        ret |= convert_uint16_to_string(adc_result3, adc_result3_txt);
-        ret |= convert_uint16_to_string(adc_result4, adc_result4_txt);
-
-        ret |= lcd_4bit_send_string_pos(&lcd_1, 1, 7, adc_result1_txt);
-        ret |= lcd_4bit_send_string_pos(&lcd_1, 2, 7, adc_result2_txt);
-        ret |= lcd_4bit_send_string_pos(&lcd_1, 3, 7, adc_result3_txt);
-        ret |= lcd_4bit_send_string_pos(&lcd_1, 4, 7, adc_result4_txt);
+        if (lm35_res2_celsius_mv > 25)
+        {
+            ret |= dc_motor_move_forward(&motor2);
+        }
+        else
+        {
+            ret |= dc_motor_stop(&motor2);
+        }
     }
    return (0);
 }
