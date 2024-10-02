f078e8416e594217ba5bce8f9614cbff6c6b89fe
Mohamed Olwi <acemido2004@gmail.com>
2024-10-02 04:41:27 +0200

    Added a new project to test the functionality of timer3 as a timer using led module to toggle each 200 ms

--- Application/Application.X/app.c
+++ Application/Application.X/app.c
@@ -6,7 +6,24 @@
  */
 #include "app.h"
 #include "device_config.h"
+#include "MCAL_Layer/Timers/Timer3/hal_timer3.h"
+void timer3_interrupt_handler(void);
 
+timer3_t timer3_obj = {
+    .timer3_preloaded_value = 3036,
+    .timer3_interrupt_handler = timer3_interrupt_handler,
+    .timer3_interrupt_priority = INTERRUPT_LOW_PRIORITY,
+    .clock_src = _TIMER3_INT_SRC,
+    .ext_clk_sync = _TIMER3_ASYNC,
+    .prescaler_value = TIMER3_PRESCALER_DIV_BY_8,
+    .rw_mode = _TIMER3_RW_16bit_OP
+};
+led_t led1 = {.port_name = PORTC_INDEX, .pin = GPIO_PIN0, .led_status = GPIO_LOW};
+
+void timer3_interrupt_handler(void)
+{
+    led_toggle(&led1);
+}
 int main(void)
 {
     Std_ReturnType ret = application_init();
@@ -14,7 +31,8 @@ int main(void)
     {
         return (-1);
     }
-
+    ret |= timer3_init(&timer3_obj);
+    ret |= led_initialize(&led1);
     while(1)
     {
        
