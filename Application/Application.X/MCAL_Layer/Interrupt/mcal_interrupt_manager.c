6c75da1ced2a3367914a96968de9ecca281c59d1
Mohamed Olwi <acemido2004@gmail.com>
2024-10-02 04:38:37 +0200

    Added timer3 to low priority interrupt manager

--- Application/Application.X/MCAL_Layer/Interrupt/mcal_interrupt_manager.c
+++ Application/Application.X/MCAL_Layer/Interrupt/mcal_interrupt_manager.c
@@ -50,6 +50,12 @@ void __interrupt() Interrupt_Manager_High(void)
  */
 void  __interrupt(low_priority) Interrupt_Manager_Low(void)
 {
+#if TIMER3_INTERRUPT_FEATURE == INTERRUPT_FEATURE_ENABLE
+    if ((INTERRUPT_ENABLE == PIE2bits.TMR3IE) && (INTERRUPT_OCCUR == PIR2bits.TMR3IF))
+    {
+        TIMER3_ISR();
+    }
+#endif
 #if TIMER2_INTERRUPT_FEATURE == INTERRUPT_FEATURE_ENABLE
     if ((INTERRUPT_ENABLE == PIE1bits.TMR2IE) && (INTERRUPT_OCCUR == PIR1bits.TMR2IF))
     {
