1bd4fbb1e8a93784dab5b72080bd978592207ff9
Mohamed Olwi <acemido2004@gmail.com>
2024-10-16 22:10:19 +0200

    Added base files for CCP1 module

---
+++ Application/Application.X/MCAL_Layer/CCP1/hal_ccp1.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   hal_ccp1.h
+ * Author: Mohamed olwi
+ *
+ * Created on 16 October 2024, 22:03
+ */
+
+#ifndef HAL_CCP1_H
+#define	HAL_CCP1_H
+/*----------------------------Header Files------------------------------------*/
+#include "../../mcal_std_types.h"
+#include <xc.h>
+#include "../../Interrupt/mcal_internal_interrupt.h"
+#include "GPIO/hal_gpio.h"
+/*----------------------------Macros Declarations-----------------------------*/
+
+/*----------------------------DataTypes---------------------------------------*/
+
+/*----------------------------Function Prototypes-----------------------------*/
+
+#endif	/* HAL_CCP1_H */
+
