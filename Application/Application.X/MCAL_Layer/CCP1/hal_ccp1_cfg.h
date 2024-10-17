fcd7dee215ec3e19aba1699d89d0c47e49e2b020
Mohamed Olwi <acemido2004@gmail.com>
2024-10-18 01:08:51 +0200

    Added a preconfig file for ccp1 module

---
+++ Application/Application.X/MCAL_Layer/CCP1/hal_ccp1_cfg.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   hal_ccp1_cfg.h
+ * Author: Mohamed olwi
+ *
+ * Created on 17 October 2024, 00:54
+ */
+
+#ifndef HAL_CCP1_CFG_H
+#define	HAL_CCP1_CFG_H
+/*----------------------------Macros Declarations-----------------------------*/
+#define CCP1_CAPTURE_MODE_CFG_SELECT           0                                /* Precompile Config for CCP1 Compare Mode */
+#define CCP1_COMPARE_MODE_CFG_SELECT           1                                /* Precompile Config for CCP1 Capture Mode */
+#define CCP1_PWM_MODE_CFG_SELECT               2                                /* Precompile Config for CCP1 PWM Mode */
+
+#define CCP1_SELECTED_MODE_CFG                 (CCP1_PWM_MODE_CFG_SELECT)       /* Select a Precompile Config for CCP1 Mode */
+/*----------------------------Macros Functions Declarations-------------------*/
+#endif	/* HAL_CCP1_CFG_H */
+
