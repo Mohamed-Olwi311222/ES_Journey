445d8aba17a4f8dcd0c67659775411f2f4409fc2
Mohamed Olwi <acemido2004@gmail.com>
2024-10-18 03:20:18 +0200

    Added precompile for timerx modules

--- Application/Application.X/MCAL_Layer/mcal_layer_cfg.h
+++ Application/Application.X/MCAL_Layer/mcal_layer_cfg.h
@@ -11,6 +11,9 @@
 #define MCAL_ENABLED                 1
 #define MCAL_DISABLED                0
 #define CCP1_MODULE_ENABLE           MCAL_ENABLED                               /* Precompile Config for CCP1 Module to enable/disable it */
+#define TIMER1_MODULE_ENABLE         MCAL_ENABLED                               /* Precompile Config for TIMER1 Module to enable/disable it */
+#define TIMER2_MODULE_ENABLE         MCAL_ENABLED                               /* Precompile Config for TIMER2 Module to enable/disable it */
+#define TIMER3_MODULE_ENABLE         MCAL_ENABLED                               /* Precompile Config for TIMER3 Module to enable/disable it */
 
 #endif	/* MCAL_LAYER_CFG_H */
 
