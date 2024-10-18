9e19e4d118261d4c3ac88da85bd419277565681d
Mohamed Olwi <acemido2004@gmail.com>
2024-10-18 03:14:11 +0200

    Added a file that contains precompile macros for each mcal layer modules to disable or enable it

---
+++ Application/Application.X/MCAL_Layer/mcal_layer_cfg.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   mcal_layer_cfg.h
+ * Author: Mohamed olwi
+ *
+ * Created on 18 October 2024, 03:07
+ */
+
+#ifndef MCAL_LAYER_CFG_H
+#define	MCAL_LAYER_CFG_H
+/*----------------------------Macros Declarations-----------------------------*/
+#define MCAL_ENABLED                 1
+#define MCAL_DISABLED                0
+#define CCP1_MODULE_ENABLE           MCAL_ENABLED                               /* Precompile Config for CCP1 Module to enable/disable it */
+
+#endif	/* MCAL_LAYER_CFG_H */
+
