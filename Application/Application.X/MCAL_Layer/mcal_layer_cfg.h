326b97143d2fe9e00baeb72d8822d5efc50776a7
Mohamed Olwi <acemido2004@gmail.com>
2024-10-28 07:23:51 +0200

    Added a preconfig for enabling CCP2 module and enabled CCP1,TIMER2 and TIMER3 modules

--- Application/Application.X/MCAL_Layer/mcal_layer_cfg.h
+++ Application/Application.X/MCAL_Layer/mcal_layer_cfg.h
@@ -10,13 +10,13 @@
 /*----------------------------Macros Declarations-----------------------------*/
 #define MCAL_ENABLED                 1
 #define MCAL_DISABLED                0
-#define GPIO_MODULE_ENABLE           MCAL_DISABLED                              /* Precompile Config for GPIO Module to enable/disable it */
-#define CCP1_MODULE_ENABLE           MCAL_DISABLED                              /* Precompile Config for CCP1 Module to enable/disable it */
-#define CCP2_MODULE_ENABLE           MCAL_ENABLED                               /* Precompile Config for CCP2 Module to enable/disable it */
+#define GPIO_MODULE_ENABLE           MCAL_ENABLED                              /* Precompile Config for GPIO Module to enable/disable it */
+#define CCP1_MODULE_ENABLE           MCAL_ENABLED                              /* Precompile Config for CCP1 Module to enable/disable it */
+#define CCP2_MODULE_ENABLE           MCAL_DISABLED                               /* Precompile Config for CCP2 Module to enable/disable it */
 #define TIMER0_MODULE_ENABLE         MCAL_DISABLED                              /* Precompile Config for TIMER0 Module to enable/disable it */
 #define TIMER1_MODULE_ENABLE         MCAL_DISABLED                              /* Precompile Config for TIMER1 Module to enable/disable it */
-#define TIMER2_MODULE_ENABLE         MCAL_DISABLED                              /* Precompile Config for TIMER2 Module to enable/disable it */
-#define TIMER3_MODULE_ENABLE         MCAL_DISABLED                              /* Precompile Config for TIMER3 Module to enable/disable it */
+#define TIMER2_MODULE_ENABLE         MCAL_ENABLED                              /* Precompile Config for TIMER2 Module to enable/disable it */
+#define TIMER3_MODULE_ENABLE         MCAL_ENABLED                              /* Precompile Config for TIMER3 Module to enable/disable it */
 #define ADC_MODULE_ENABLE            MCAL_DISABLED                              /* Precompile Config for ADC Module to enable/disable it */
 #define EEPROM_MODULE_ENABLE         MCAL_DISABLED                              /* Precompile Config for EEPROM Module to enable/disable it */
 #endif	/* MCAL_LAYER_CFG_H */
