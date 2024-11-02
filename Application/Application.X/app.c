e225246054e363cb9b92f656f656747567209870
Mohamed Olwi <acemido2004@gmail.com>
2024-11-02 10:04:32 +0200

    Cleaned app.c

--- Application/Application.X/app.c
+++ Application/Application.X/app.c
@@ -6,6 +6,10 @@
  */
 #include "app.h"
 #include "device_config.h"
+#include "MCAL_Layer/CCPx/CCP1/hal_ccp1.h"
+#include "MCAL_Layer/Timers/Timer3/hal_timer3.h"
+#include "MCAL_Layer/Timers/Timer2/hal_timer2.h"
+
 
 int main(void)
 {
@@ -17,4 +21,3 @@ int main(void)
     }
     return (ret);
 }
-
