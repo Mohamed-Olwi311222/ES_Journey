314e5266005b3ac21bbbd7c31a024c65df538696
Mohamed Olwi <acemido2004@gmail.com>
2024-12-02 13:19:45 +0200

    Deleted the unused macros

--- Application/Application.X/MCAL_Layer/EUSART/hal_eusart_cfg.h
+++ Application/Application.X/MCAL_Layer/EUSART/hal_eusart_cfg.h
@@ -11,10 +11,5 @@
 #define EUSART_SYNC_MODE                    1                           /* EUSART SYNC MODE */
 #define EUSART_ASYNC_MODE                   0                           /* EUSART ASYNC MODE */
 #define EUSART_ACTIVE_MODE                  (EUSART_ASYNC_MODE)         /* Which EUSART mode is active */
-#if EUSART_ACTIVE_MODE == EUSART_SYNC_MODE
-#define EUSART_SYNC_MASTER_MODE             1                           /* EUSART SYNC MODE MASTER */
-#define EUSART_SYNC_SLAVE_MODE              0                           /* EUSART SYNC MODE SLAVE */
-#define EUSART_SYNC_MODE                    (EUSART_SYNC_MASTER_MODE)   /* Which sync mode is active */
-#endif
 #endif	/* HAL_EUSART_CFG_H */
 
