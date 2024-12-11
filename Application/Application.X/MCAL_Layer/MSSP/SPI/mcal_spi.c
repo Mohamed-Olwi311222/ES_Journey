2ce614c9aca209b0d77f8e35dc8795cd3b7c5f81
Mohamed-Olwi311222 <mohamedolwi38@gmail.com>
2024-12-12 00:38:38 +0200

    Added a new dir for SPI mode in MSSP module

---
+++ Application/Application.X/MCAL_Layer/MSSP/SPI/mcal_spi.c
@@ -0,0 +1,6 @@
+/* 
+ * File:   mcal_spi.c
+ * Author: Mohamed olwi
+ *
+ * Created on 12 December 2024, 00:36
+ */
---
+++ Application/Application.X/MCAL_Layer/MSSP/SPI/mcal_spi.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   mcal_spi.h
+ * Author: Mohamed olwi
+ *
+ * Created on 12 December 2024, 00:36
+ */
+
+#ifndef MCAL_SPI_H
+#define	MCAL_SPI_H
+/*----------------------------Header Files------------------------------------*/
+
+/*----------------------------Macros Declarations-----------------------------*/
+/*----------------------------Macros Functions Declarations-------------------*/
+
+#endif	/* MCAL_SPI_H */
+
