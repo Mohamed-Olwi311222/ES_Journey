6008be29bfb644093a98d9c6c45ba38f7c2e61d6
Mohamed-Olwi311222 <mohamedolwi38@gmail.com>
2024-12-12 13:33:18 +0200

    added some sections to the source file and added a template for the first function to implement

--- Application/Application.X/MCAL_Layer/MSSP/SPI/mcal_spi.c
+++ Application/Application.X/MCAL_Layer/MSSP/SPI/mcal_spi.c
@@ -4,3 +4,33 @@
  *
  * Created on 12 December 2024, 00:36
  */
+#include "mcal_spi.h"
+
+/*---------------Static Data types----------------------------------------------*/
+
+/*---------------Static Data types End------------------------------------------*/
+
+/*---------------Static Helper functions declerations---------------------------*/
+
+/*---------------Static Helper functions declerations End-----------------------*/
+
+
+/**
+ * @brief: Initialize the SPI module
+ * @param spi_obj the SPI module object
+ * @return E_OK if success otherwise E_NOT_OK
+ */
+Std_ReturnType spi_init(const spi_t *const spi_obj)
+{
+    Std_ReturnType ret = E_OK;
+    
+    if (NULL == spi_obj)
+    {
+        ret = E_NOT_OK;
+    }
+    else
+    {
+        ret = E_OK;
+    }
+    return (ret);
+}
